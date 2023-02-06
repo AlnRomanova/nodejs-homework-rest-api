8d784b1499215d9577e4f9bb13ac91f6e8eb8f77
Alina Romanova <alinakhraban@gmail.com>
2023-02-06 16:04:30 +0200

    added edits

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,8 +1,6 @@
 const { globalErrorHandler } = require('./global-error-handler.middleware');
 const { validateBody, updateValidateBody} = require('./validate-body.middleware');
 
-
-
 module.exports = {
   globalErrorHandler,
   validateBody,
